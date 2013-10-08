68e5c7b2950154b270c22f55221da775be642704
Leandro Ponte <alu0100724165@ull.edu.es>
2013-10-09 00:10:03 +0100

    funcion multiplicacion

--- matrices/matrices.rb
+++ matrices/matrices.rb
@@ -18,3 +18,24 @@ def mostrar_Matriz(matriz)
 	end
 
 end
+
+=begin
+Este es el método para Multiplicar
+=end
+
+def multiplicar_Matriz(matrizA, matrizB, tam)
+
+multi=Array.new(tam){Array.new(tam)}
+
+	for i in (0..matrizA.length-1)	
+		for j in (0..matrizB.length-1)
+			for k in (0..multi.length-1)
+			multi[i][j]=(multi[i][j].to_i + (matrizA[i][k].to_i * matrizB[k][j].to_i)) 
+			end
+		end	
+	end
+
+return multi
+end
+
+
--- matrices/matrices.rb~
+++ matrices/matrices.rb~
@@ -1,1 +1,20 @@
 #!/usr/bin/ruby
+
+
+=begin
+Este es el método para mostrar el array
+=end
+def mostrar_Matriz(matriz)
+	for i in(0..matriz.length-1)
+		
+		for x in(0..matriz[0].length-1)
+			
+			print "#{matriz[i][x]}|"
+			
+			x=x+1
+		end
+		puts "\n"
+		i=i+1
+	end
+
+end
